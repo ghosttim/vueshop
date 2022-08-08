ad3bcb6c049cb37ff7ac1251529b0d816f8c9200
KurganovArtem <a.kurganov@youtravel.me>
2022-08-08 09:46:30 +0300

    lesson-5. remove views

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import App from './App.vue';
-import router from './router';
 import store from './store';
 
 import {titleName, data} from './data';
@@ -9,7 +8,6 @@ import send from './send';
 Vue.config.productionTip = false;
 
 new Vue({
-  router,
   store,
   render: (h) => h(App),
 }).$mount('#app');
