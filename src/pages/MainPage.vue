5c46d5b41ee121e3586c4e30abc2434638fe6de3
KurganovArtem <a.kurganov@youtravel.me>
2022-10-15 14:15:26 +0300

    lesson-7.3 show products from api

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -33,6 +33,7 @@ import products from '@/data/products';
 import ProductList from '@/components/ProductList';
 import BasePagination from '@/components/BasePagination';
 import ProductFilter from '@/components/ProductFilter';
+import axios from 'axios';
 
 export default {
   components: {
@@ -48,6 +49,7 @@ export default {
       filterColorId: 0,
       page: 1,
       productsPerPage: 3,
+      productsData: null,
     }
   },
   computed: {
@@ -73,12 +75,32 @@ export default {
       return filteredProducts;
     },
     products() {
-      const offset = (this.page - 1) * this.productsPerPage;
-      return this.filteredProducts.slice(offset, offset + this.productsPerPage);
+      return this.productsData
+        ? this.productsData.items.map(product => {
+          return {
+            ...product,
+            image: product.image.file.url,
+          }
+        })
+        : [];
     },
     countProducts() {
-      return this.filteredProducts.length;
+      return this.productsData ? this.productsData.pagination.total : 0;
     },
+  },
+  methods: {
+    loadProducts() {
+      axios.get(`https://vue-study.skillbox.cc/api/products?page=${this.page}&limit=${this.productsPerPage}`)
+        .then(response => this.productsData = response.data);
+    }
+  },
+  watch: {
+    page() {
+      this.loadProducts()
+    }
+  },
+  created() {
+    this.loadProducts()
   }
 };
 </script>
